91854e5c320d2991f0e8ac5b8d9a1c7928b6d717
varenka <varya.starovoytova@gmail.com>
2026-08-05 12:41:11 +1000

    fix import sorting

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,6 +1,5 @@
 from src.processing import filter_by_state, sort_by_date
 
-
 operations = [
     {"id": 1, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
     {"id": 2, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
